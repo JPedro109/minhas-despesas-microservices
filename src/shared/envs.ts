f4989ec776597ae2866832e1dfdb0f463099b3c7
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-10 20:44:35 -0300

    chore: add environment variables

--- src/shared/envs.ts
+++ src/shared/envs.ts
@@ -5,6 +5,9 @@ type EnvironmentVariables = {
     awsAccessKeyId: string;
     secretAccessKey: string;
     awsRegion: string;
+    awsCognitoClientId: string;
+    awsCognitoClientSecret: string;
+    awsCognitoUserPoolId: string;
 };
 
 export const envs: EnvironmentVariables = {
@@ -12,4 +15,7 @@ export const envs: EnvironmentVariables = {
     awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     awsRegion: process.env.AWS_REGION,
+    awsCognitoClientId: process.env.AWS_COGNITO_CLIENT_ID,
+    awsCognitoClientSecret: process.env.AWS_COGNITO_CLIENT_SECRET,
+    awsCognitoUserPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
 };
