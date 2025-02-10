4db3ec878164f91a69b00983a0ee4c7c12c00e0a
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-10 20:43:54 -0300

    feat: add custom errors

---
+++ src/shared/errors/index.ts
@@ -0,0 +1,5 @@
+export * from "./services/conflicted.error";
+export * from "./services/forbidden.error";
+export * from "./services/invalid-param.error";
+export * from "./services/not-found.error";
+export * from "./services/unauthorized.error";
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,2 +1,3 @@
 export * from "./envs";
 export * from "./aws";
+export * from "./errors";
