dc28e85edde9963836b4545d28e8c37d5a2f5200
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-11 21:00:53 -0300

    feat: add send email account update email service

--- src/microservices/account/services/index.ts
+++ src/microservices/account/services/index.ts
@@ -2,3 +2,4 @@ export * from "./create-account.service";
 export * from "./verify-account-email.service";
 export * from "./account-login.service";
 export * from "./refresh-account-token.service";
+export * from "./send-email-account-update-email.service";
---
+++ src/microservices/account/services/send-email-account-update-email.service.ts
@@ -0,0 +1,44 @@
+import { NotFoundError } from "@/shared";
+import { IdentityProvider } from "../infrastructure";
+
+export type SendAccountEmailUpdateEmailDTO = {
+    identityProviderId: string;
+    newEmail: string;
+};
+
+export type SendAccountEmailUpdateEmailResponseDTO = {
+    accessToken: string;
+    refreshToken: string;
+};
+
+export const sendAccountEmailUpdateEmailSchema = {
+    identityProviderId: {
+        type: "string",
+        optional: false,
+    },
+    newEmail: {
+        type: "string",
+        optional: false,
+    },
+};
+
+export class SendAccountEmailUpdateEmailService {
+    constructor(private readonly identityProvider: IdentityProvider) {}
+
+    async execute({
+        identityProviderId,
+        newEmail,
+    }: SendAccountEmailUpdateEmailDTO): Promise<void> {
+        const accountInfo =
+            await this.identityProvider.getAccountInfo(identityProviderId);
+
+        if (!accountInfo) {
+            throw new NotFoundError("A conta não exite");
+        }
+
+        await this.identityProvider.sendEmailEmailUpdate(
+            accountInfo.email,
+            newEmail,
+        );
+    }
+}
