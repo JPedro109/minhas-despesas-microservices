fd56852826e7202966a0a3c930cca3d351c3b22f
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-19 21:28:58 -0300

    feat: add update payment method token service

--- src/microservices/subscription/services/payment-method/index.ts
+++ src/microservices/subscription/services/payment-method/index.ts
@@ -2,3 +2,4 @@ export * from "./create-payment-method.service";
 export * from "./delete-payment-method.service";
 export * from "./get-account-payment-method.service";
 export * from "./update-payment-method-name.service";
+export * from "./update-payment-method-token.service";
---
+++ src/microservices/subscription/services/payment-method/update-payment-method-token.service.ts
@@ -0,0 +1,74 @@
+import { NotFoundError, RequestSchema } from "@/shared";
+import { PaymentMethodDAO, CustomerDAO, Payment } from "../../infrastructure";
+
+export type UpdatePaymentMethodTokenDTO = {
+    accountId: string;
+    token: string;
+};
+
+export const updatePaymentMethodTokenSchema: RequestSchema = {
+    accountId: {
+        type: "string",
+        optional: false,
+    },
+    token: {
+        type: "string",
+        optional: false,
+    },
+};
+
+export class UpdatePaymentMethodTokenService {
+    constructor(
+        private readonly paymentMethodDAO: PaymentMethodDAO,
+        private readonly customerDAO: CustomerDAO,
+        private readonly payment: Payment,
+    ) {}
+
+    async execute({
+        accountId,
+        token,
+    }: UpdatePaymentMethodTokenDTO): Promise<void> {
+        const paymentMethod =
+            await this.paymentMethodDAO.getPaymentMethodByAccountId(accountId);
+        if (!paymentMethod) {
+            throw new NotFoundError("Esse método de pagamento não existe");
+        }
+        const oldToken = paymentMethod.token;
+
+        const customer =
+            await this.customerDAO.getCustomerByAccountId(accountId);
+        const tokenCreated =
+            await this.payment.attachmentPaymentMethodInCustomer(
+                customer.customerId,
+                token,
+            );
+
+        paymentMethod.token = tokenCreated;
+        await this.paymentMethodDAO.updatePaymentMethodByAccountIdAndPaymentMethodId(
+            paymentMethod.accountId,
+            paymentMethod.paymentMethodId,
+            paymentMethod,
+        );
+
+        try {
+            await this.payment.detachmentPaymentMethodInCustomerByToken(
+                oldToken,
+            );
+            await this.payment.payExpiredSubscriptionIfAny(
+                customer.customerId,
+                tokenCreated,
+            );
+        } catch (e) {
+            paymentMethod.token = oldToken;
+            await this.paymentMethodDAO.updatePaymentMethodByAccountIdAndPaymentMethodId(
+                paymentMethod.accountId,
+                paymentMethod.paymentMethodId,
+                paymentMethod,
+            );
+            await this.payment.detachmentPaymentMethodInCustomerByToken(
+                tokenCreated,
+            );
+            throw e;
+        }
+    }
+}
