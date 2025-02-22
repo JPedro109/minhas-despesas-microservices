86a5cd9202aa7cb93fdfe6b1876f822dbdd97605
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-21 22:10:27 -0300

    refactor: rename file service

--- src/microservices/subscription/services/subscription/index.ts
+++ src/microservices/subscription/services/subscription/index.ts
@@ -1,4 +1,4 @@
 export * from "./create-subscription.service";
 export * from "./get-account-subscription.service";
 export * from "./update-subscription-renewal-status.service";
-export * from "./notify-user-of-subscription-payment-failure.service";
+export * from "./notify-account-of-subscription-payment-failure.service";
