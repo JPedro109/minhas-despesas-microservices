7546c2e0eda9ca7c0104a50429bbe6c8ad3fa054
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-23 17:58:28 -0300

    feat: add pay expense service

--- src/microservices/expense/services/expense/index.ts
+++ src/microservices/expense/services/expense/index.ts
@@ -2,3 +2,4 @@ export * from "./create-expense.service";
 export * from "./get-account-expenses.service";
 export * from "./update-expense.service";
 export * from "./delete-expense.service";
+export * from "./pay-expense.service";
