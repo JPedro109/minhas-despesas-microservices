eac4a81960b7e7f8c6e130243e61fb6150bb97b7
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-23 17:43:09 -0300

    feat: add delete expense service

---
+++ src/microservices/expense/services/expense/delete-expense.service.ts
@@ -0,0 +1,46 @@
+import { NotFoundError } from "@/shared";
+import { ExpenseDAO, PaymentHistoryDAO } from "../../infrastructure";
+
+export type DeleteExpenseDTO = {
+    accountId: string;
+    expenseId: string;
+    deleteExpensePaymentHistory: boolean;
+};
+
+export class DeleteExpenseUseCase {
+    constructor(
+        private readonly expenseDAO: ExpenseDAO,
+        private readonly paymentHistoryDAO: PaymentHistoryDAO,
+    ) {}
+
+    async execute({
+        accountId,
+        expenseId,
+        deleteExpensePaymentHistory,
+    }: DeleteExpenseDTO): Promise<void> {
+        const expense =
+            await this.expenseDAO.getExpensesByAccountIdAndExpenseId(
+                accountId,
+                expenseId,
+            );
+        if (!expense) {
+            throw new NotFoundError("Essa despesa não existe");
+        }
+
+        await this.expenseDAO.deleteExpenseByAccountIdAndExpenseId(
+            accountId,
+            expenseId,
+        );
+
+        try {
+            if (deleteExpensePaymentHistory) {
+                await this.paymentHistoryDAO.deletePaymentHistoriesByAccountId(
+                    accountId,
+                );
+            }
+        } catch (e) {
+            await this.expenseDAO.createExpense(expense);
+            throw e;
+        }
+    }
+}
--- src/microservices/expense/services/expense/index.ts
+++ src/microservices/expense/services/expense/index.ts
@@ -1,3 +1,4 @@
 export * from "./create-expense.service";
 export * from "./get-account-expenses.service";
 export * from "./update-expense.service";
+export * from "./delete-expense.service";
