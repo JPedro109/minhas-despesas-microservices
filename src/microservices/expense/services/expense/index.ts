44d2e20ebdf4295d8c3e34ff9d1db8c92eedfabb
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-23 19:58:34 -0300

    feat: add update previous month paid expenses to unpaid service

--- src/microservices/expense/services/expense/index.ts
+++ src/microservices/expense/services/expense/index.ts
@@ -4,3 +4,4 @@ export * from "./update-expense.service";
 export * from "./delete-expense.service";
 export * from "./pay-expense.service";
 export * from "./expense-undo-payment.service";
+export * from "./update-previous-month-paid-expenses-to-unpaid.service";
---
+++ src/microservices/expense/services/expense/update-previous-month-paid-expenses-to-unpaid.service.ts
@@ -0,0 +1,33 @@
+import { ExpenseDAO } from "../../infrastructure";
+
+export class UpdatePreviousMonthPaidExpensesToUnpaidService {
+    constructor(private readonly expenseDAO: ExpenseDAO) {}
+
+    async execute(): Promise<void> {
+        const dateWithPreviousMonth = new Date();
+        dateWithPreviousMonth.setMonth(dateWithPreviousMonth.getUTCMonth() - 1);
+        const expenses =
+            await this.expenseDAO.getExpensesByExpenseDueYearAndExpenseDueMonthAndPaymentStatus(
+                dateWithPreviousMonth.getUTCFullYear(),
+                dateWithPreviousMonth.getUTCDate(),
+                true,
+            );
+
+        const updatedExpenses = expenses.map((x) => {
+            x.expenseDueDate.setMonth(x.expenseDueDate.getUTCMonth() + 1);
+
+            return {
+                ...x,
+                paid: false,
+            };
+        });
+
+        await this.expenseDAO.updateExpensesByAccountIdAndExpenseId(
+            updatedExpenses.map((x) => ({
+                accountId: x.accountId,
+                expenseId: x.expenseId,
+                data: x,
+            })),
+        );
+    }
+}
