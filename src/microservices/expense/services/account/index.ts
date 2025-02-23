baa99b6d2fb94933d76dd1051fb9313311d24557
João Pedro <joaopedromoraesnsilva@gmail.com>
2025-02-23 16:40:03 -0300

    feat: add update account email

--- src/microservices/expense/services/account/index.ts
+++ src/microservices/expense/services/account/index.ts
@@ -1,1 +1,2 @@
 export * from "./create-account.service";
+export * from "./update-account-email.service";
---
+++ src/microservices/expense/services/account/update-account-email.service.ts
@@ -0,0 +1,34 @@
+import { NotFoundError, RequestSchema } from "@/shared";
+import { AccountDAO } from "../../infrastructure";
+
+export type UpdateAccountDTO = {
+    accountId: string;
+    email: string;
+};
+
+export const updateAccountSchema: RequestSchema = {
+    accountId: {
+        type: "string",
+        optional: false,
+    },
+    email: {
+        type: "string",
+        optional: false,
+    },
+};
+
+export class UpdateAccountService {
+    constructor(private readonly accountDAO: AccountDAO) {}
+
+    async execute({ accountId, email }: UpdateAccountDTO): Promise<void> {
+        const account = await this.accountDAO.getAccountById(accountId);
+
+        if (!account) {
+            throw new NotFoundError("A conta não existe");
+        }
+
+        account.email = email;
+
+        await this.accountDAO.updateAccountById(accountId, account);
+    }
+}
